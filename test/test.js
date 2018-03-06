c6ced91a7b8a687e5ef6d9a64a9b8a7b1acf3341
Eterion <Eterion@users.noreply.github.com>
2018-03-06 07:09:34 +0100

    Added script for sass-true to run mocha test on scss files

---
+++ test/test.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const sassTrue = require('sass-true');
+
+(function test(dir) {
+  fs.readdirSync(dir).forEach(file => {
+
+    // isDirectory
+    if(fs.statSync(path.join(dir, file)).isDirectory()) {
+      describe(file, function() {
+        test(path.join(dir, file));
+      });
+    }
+
+    // runSass
+    else {
+      if(path.extname(file) == '.scss') {
+        sassTrue.runSass({ file: path.join(dir, file) }, describe, it);
+      }
+    }
+
+  });
+})(path.resolve(__dirname));
