fed2969a85fa1f3fd41584b710e34d7ae6d09036
Eterion <Eterion@users.noreply.github.com>
2018-09-17 12:58:38 +0200

    New config and modified styles

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,19 @@
-require('@babel/register');
-const config = require('./config.es6').default;
-module.exports = config;
+module.exports = {
+  base: `/${process.env.npm_package_name}/`,
+  description: process.env.npm_package_description,
+  evergreen: true,
+  themeConfig: {
+    docsDir: 'docs',
+    editLinks: true,
+    lastUpdated: true,
+    nav: [
+      { text: 'Guide', link: '/guide' },
+      { text: 'Constants', link: '/constants' },
+      { text: 'Functions', link: '/functions' },
+      { text: 'Mixins', link: '/mixins' },
+    ],
+    repo: 'Eterion/esm-scss',
+    sidebar: 'auto',
+  },
+  title: `ESM Scss`,
+};
