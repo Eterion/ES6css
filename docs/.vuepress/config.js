c92a43181197ea8c6d1a1400129c0cbfd6233597
Eterion <Eterion@users.noreply.github.com>
2018-09-21 07:04:32 +0200

    Google Analytics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   base: `/${process.env.npm_package_name}/`,
   description: process.env.npm_package_description,
   evergreen: true,
+  ga: 'UA-126139204-1',
   themeConfig: {
     docsDir: 'docs',
     editLinks: true,
